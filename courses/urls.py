85366952ad34de2b9a5c4b6d32f213faba20e178
Vitor Lima <viitorlima2000@gmail.com>
2025-04-14 17:32:43 -0300

    Update urls.py

--- courses/urls.py
+++ courses/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from courses.views import CourseAPIView, RatingAPIView
+from courses.views import CourseAPIView, RatingAPIView, CoursesAPIView, RatingsAPIView
 
 urlpatterns = [ # Endpoints
-    path('courses/', CourseAPIView.as_view(), name='courses'),
+    path('courses/', CoursesAPIView.as_view(), name='courses'),
 
-    path('ratings/', RatingAPIView.as_view(), name='ratings'),
+    path('courses/<int:pk>/', CourseAPIView.as_view(), name='course'),
+
+    path('ratings/', RatingsAPIView.as_view(), name='ratings'),
+    
+    path('ratings/<int:pk>/', RatingAPIView.as_view(), name='rating'),
 ]
