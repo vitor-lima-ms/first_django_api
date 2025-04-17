8a225a69f5014fec89cefb1124623619d3a6ca48
Vitor Lima <viitorlima2000@gmail.com>
2025-04-17 20:26:31 -0300

    Update urls.py

--- base_dir/urls.py
+++ base_dir/urls.py
@@ -17,10 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from courses.urls import router
+
 urlpatterns = [
+
+    path('api/v1/', include('courses.urls')),
+
+    path('api/v2/', include(router.urls)), # URLs geradas automaticamente pelo router
+
     path('admin/', admin.site.urls),
 
     path('auth/', include('rest_framework.urls')),
-
-    path('api/v1/', include('courses.urls')),
 ]
