c1a05c26b9ad15b480daa93f0da2a6b3720faf07
Vitor Lima <viitorlima2000@gmail.com>
2025-04-17 20:26:37 -0300

    Update serializers.py

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,13 +1,8 @@
 from rest_framework import serializers
 from courses.models import Course, Rating
 
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        
-        fields = '__all__' # Campos que serão exibidos
-
 class RatingSerializer(serializers.ModelSerializer):
+
     class Meta:
         extra_kwargs = {
             'email': {'write_only': True}, # Email não será exibido. Será exigido apenas para se cadastrar
@@ -16,3 +11,13 @@ class RatingSerializer(serializers.ModelSerializer):
         model = Rating
         
         fields = '__all__'
+
+class CourseSerializer(serializers.ModelSerializer):
+    # ratings = RatingSerializer(many=True, read_only=True) # Nested relashionship --> Permite exibir as avaliações no request de exibe os cursos. Muito bom para relacionamentos One to One
+    ratings = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='rating-detail') # Hyperlinked relashionship --> Permite exibir as avaliações no request de exibe os cursos. Muito bom para relacionamentos One to Many. Modo mais recomendado para API's REST. O nome tem que ser exatamente o nome do modelo-detail
+    # ratings = serializers.PrimaryKeyRelatedField(many=True, read_only=True) # PrimaryKey relashionship --> Apresente a PK das avaliações no request de exibe os cursos. Mais performática.
+
+    class Meta:
+        model = Course
+        
+        fields = '__all__' # Campos que serão exibidos
