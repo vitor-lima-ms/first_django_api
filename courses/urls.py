33cdb732de94c7764d41d2fc266b8a718f8a5fd2
Vitor Lima <viitorlima2000@gmail.com>
2025-04-17 17:01:54 -0300

    Update urls.py

--- courses/urls.py
+++ courses/urls.py
@@ -4,9 +4,13 @@ from courses.views import CourseAPIView, RatingAPIView, CoursesAPIView, RatingsA
 urlpatterns = [ # Endpoints
     path('courses/', CoursesAPIView.as_view(), name='courses'),
 
-    path('courses/<int:pk>/', CourseAPIView.as_view(), name='course'),
+    path('courses/<int:pk>/', CourseAPIView.as_view(), name='course'), # Não precisamos sobrescrever nenhum método dessa view, logo podemos usar apenas pk na URL
+
+    path('courses/<int:course_pk>/ratings/', RatingsAPIView.as_view(), name='course_ratings'),
+
+    path('courses/<int:course_pk>/ratings/<int:rating_pk>/', RatingAPIView.as_view(), name='course_rating'), # Aqui temos que diferenciar entre as pks do curso e da avaliação, então usamos course_pk e rating_pk na URL
 
     path('ratings/', RatingsAPIView.as_view(), name='ratings'),
     
-    path('ratings/<int:pk>/', RatingAPIView.as_view(), name='rating'),
+    path('ratings/<int:rating_pk>/', RatingAPIView.as_view(), name='rating'), # Como já vamos sobrescrever a view, também podemos mudar de pk para rating_pk, assim como fizemos na URL acima
 ]
