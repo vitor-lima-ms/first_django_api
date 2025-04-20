8bba8a95c078ffbb651a45e34cfce4aff18625cb
Vitor Lima <viitorlima2000@gmail.com>
2025-04-20 10:59:57 -0300

    Update settings.py

--- base_dir/settings.py
+++ base_dir/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     
     'django_filters',
     'rest_framework',
-    
+    'rest_framework.authtoken',
+
     'courses',
 ]
 
@@ -134,7 +135,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
@@ -142,3 +144,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2,
 }
+
+# 72558c761d55e21db1417e88f740e5a45bc455c7 --> Token gerado
